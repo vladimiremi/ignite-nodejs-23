f4aba9a9780580392e4483009113ec5c1f82d48b
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2023-06-26 17:19:54 -0300

    feat: create application

---
+++ src/server.spec.ts
@@ -0,0 +1,7 @@
+import {describe, expect, test} from 'vitest'
+
+describe('teste', ()=>{
+    test('testando mesmo', ()=>{
+        expect(1 + 1).toEqual(2)
+    })
+})
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/hello', ()=>{
+    return 'Hello World!'
+})
+
+app.listen({
+    port: 3333
+}).then(()=>console.log('Server Running!'))
