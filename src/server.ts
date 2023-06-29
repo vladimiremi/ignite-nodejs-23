9a2b43b4bc4ba7a2197aae022a7e9e25bb3a45bb
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-06-29 15:40:14 -0300

    feat: add query with `knex`

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+  return transactions
 })
 
 app
