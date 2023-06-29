aad6daa77feae63c66ec33cf23fb08de9cbe644a
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-06-29 15:41:35 -0300

    chore: add environment variables

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import { knex as setupKnex, Knex } from 'knex'
+import 'dotenv/config'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('env DATABASE_URL not found')
+}
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
