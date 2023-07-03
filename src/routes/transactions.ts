2dead3d107dc333ca00900c3b467c1868758ec9e
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-03 09:07:10 -0300

    feat: plugins from `Fastify`

---
+++ src/routes/transactions.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+
+export async function transactions(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const transactions = await knex('transactions')
+      .where('amount', 1000)
+      .select('*')
+    return transactions
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactions } from './routes/transactions'
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-  return transactions
-})
+app.register(transactions)
 
 app
   .listen({
