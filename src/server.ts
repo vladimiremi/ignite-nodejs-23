f448cf9dcc912bcf74a604145c132c4171e38297
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-11 14:51:54 -0300

    feat: use cookies on Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
 import { env } from './env'
 import { transactions } from './routes/transactions'
 const app = fastify()
+
+app.register(cookie)
 
 app.register(transactions, {
   prefix: 'transactions',
