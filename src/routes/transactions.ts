ea8896bb6d9a807639ffe65d55cb11e99ed8562a
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-11 14:16:15 -0300

    feat: add transactions list

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,26 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 
 export async function transactions(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
