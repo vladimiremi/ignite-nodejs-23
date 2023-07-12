2cfeda375eb9b1a2107e9ca698e0e5fa7be4aa99
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-12 11:36:46 -0300

    test: categorize the testes

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { it, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('User can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
