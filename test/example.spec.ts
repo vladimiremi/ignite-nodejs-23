8e926f03afe1fa7650302d302cd6f55ecdda096a
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-12 11:23:19 -0300

    test: add test of create transaction

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toBe(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('User can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
