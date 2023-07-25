c1fc841098559d2e829ba4d43fc79fdbac1840a8
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-25 08:52:04 -0300

    feat: add register use case

--- 03-api-SOLID/src/http/controllers/register.ts
+++ 03-api-SOLID/src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { registerUseCase } from '@/use-cases/register'
 import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
@@ -12,25 +13,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send()
-  }
-
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUseCase({
       name,
       email,
-      password_hash,
-    },
-  })
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
 
   return reply.status(201).send()
 }
