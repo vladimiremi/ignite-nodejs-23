c699a33e8e4c15a89460b01dece6f85b804af9a8
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-24 23:22:31 -0300

    feat: add create user route

--- 03-api-SOLID/src/app.ts
+++ 03-api-SOLID/src/app.ts
@@ -1,13 +1,25 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Vladimir',
-    email: 'vladimir@gmail.com',
-  },
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
