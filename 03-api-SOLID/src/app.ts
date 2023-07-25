da560af2c59a7d2b69bf3538ad805fef7cb5c8a0
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-07-25 13:41:52 -0300

    feat: handler of global errors

--- 03-api-SOLID/src/app.ts
+++ 03-api-SOLID/src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external took like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
 app.register(appRoutes)
