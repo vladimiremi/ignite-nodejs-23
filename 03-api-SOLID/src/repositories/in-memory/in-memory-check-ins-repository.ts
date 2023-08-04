74775297182b4bc3034e6a4ac2b36ac1b92b90ea
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-08-04 14:31:45 -0300

    feat: add history use case

--- 03-api-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 03-api-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -27,6 +27,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkOnSameDate
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((checkIn) => checkIn.userId === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
