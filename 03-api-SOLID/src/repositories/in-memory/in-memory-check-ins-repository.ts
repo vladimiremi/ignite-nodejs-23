5b794bb9fb3f921a7b9b3d9ece94aaa1646a6c1a
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-08-04 08:33:28 -0300

    test: validate check in different days

--- 03-api-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 03-api-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,15 +1,24 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { Prisma, CheckIn } from '@prisma/client'
 import { GetResult } from '@prisma/client/runtime/library'
+import dayjs from 'dayjs'
 import { randomUUID } from 'node:crypto'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const checkOnSameDate = this.items.find(
-      (chekin) => chekin.userId === userId,
-    )
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkOnSameDate = this.items.find((chekin) => {
+      const checkInDate = dayjs(chekin.created_at)
+
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return chekin.userId === userId && isOnSameDate
+    })
 
     if (!checkOnSameDate) {
       return null
