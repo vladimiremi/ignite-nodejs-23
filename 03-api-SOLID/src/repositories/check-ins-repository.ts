46436bb35b468f73abff2d472d465ccd32564325
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-08-04 14:38:33 -0300

    feat: add metrics use case

--- 03-api-SOLID/src/repositories/check-ins-repository.ts
+++ 03-api-SOLID/src/repositories/check-ins-repository.ts
@@ -3,5 +3,6 @@ import { CheckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  countByUserId(userId: string): Promise<number>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
 }
--- 03-api-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 03-api-SOLID/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -33,6 +33,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .slice((page - 1) * 20, page * 20)
   }
 
+  async countByUserId(userId: string) {
+    return this.items.filter((checkIn) => checkIn.userId === userId).length
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
