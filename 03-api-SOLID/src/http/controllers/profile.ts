3f8821ae48e786fb652f00497dd1df896c249a88
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-08-18 10:22:44 -0300

    feat: add profile controller

--- 03-api-SOLID/src/http/controllers/profile.ts
+++ 03-api-SOLID/src/http/controllers/profile.ts
@@ -1,6 +1,18 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
-  return reply.status(200).send()
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  Reflect.deleteProperty(user, 'password_hash')
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+    },
+  })
 }
