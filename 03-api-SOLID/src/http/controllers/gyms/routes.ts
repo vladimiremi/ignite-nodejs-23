20b9aade713a084dd1af578c9e1477a10d614aa0
José Vladimir Costa Alves <vladimiremi2014@gmail.com>
2023-09-05 15:26:12 -0300

    feat: add authorization by roles `RBAC`

--- 03-api-SOLID/src/http/controllers/gyms/routes.ts
+++ 03-api-SOLID/src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { nearby } from './nearby'
 import { search } from './search'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
@@ -10,5 +11,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
